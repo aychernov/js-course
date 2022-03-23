5cdb22d86fffa220f4e59712876055013cd5dca4
tallboy <yourspaceworkteam@gmail.com>
2022-03-23 23:56:47 +0300

    edit readme

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-let a = {}
+const a = [] === []
+const b = [] == []
+const c = [1, 2, 3] === [1, 2, 3]
+const d = [1, 8, 5] === [1, 2, 3]
 
-console.log(!!a)
+const check = () => {
+  if (a && b && d) {
+    return 'all passed'
+  }
+  if (a || b || c) {
+    return 'some passed'
+  }
+  return 'fail'
+}
+
+console.log(check());
