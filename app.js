25553abecf5b9662531553df8d0fe9adde65d17f
tallboy <yourspaceworkteam@gmail.com>
2022-04-06 19:50:33 +0300

    end some lessons

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-const a = [] === []
-const b = [] == []
-const c = [1, 2, 3] === [1, 2, 3]
-const d = [1, 8, 5] === [1, 2, 3]
+const fn = (a, b = 20) => a * b
 
-const check = () => {
-  if (a && b && d) {
-    return 'all passed'
-  }
-  if (a || b || c) {
-    return 'some passed'
-  }
-  return 'fail'
-}
-
-console.log(check());
+console.log(fn(22));
